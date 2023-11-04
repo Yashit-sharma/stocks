6047eee51fe9b12238ccffdc933ac2c3b6c48bce
yashit sharma <yashitsharma@yashits-MacBook-Air.local>
2023-11-04 15:17:00 +0530

    first commit

--- src/Components/Stocks.js
+++ src/Components/Stocks.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../Styles/Stocks.css'
 import { Link } from 'react-router-dom'
-import val from '../Components/Data.js'
+import val from './Data.js'
 export default function Stocks({ticker_Name}) {
   const Data = val;
   const handleClick = (ticker)=>{
